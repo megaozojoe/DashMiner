c0277f14ca689126d03b7f2d7ec0ce036bb3ec49
Matthew del Real <matthewadelreal@gmail.com>
2023-08-11 19:41:19 -0400

    Adding in files
    Starting code :)

---
+++ src/DashMiner.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+
+public class DashMiner {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
